ec7c430e5b656f94b4c1b022a8b10374867cd87b
abdelahdiDevweb <abdouhadi2002@gmail.com>
2025-04-09 16:52:57 +0100

    O

---
+++ TypeORM/TypeORM.ts
@@ -0,0 +1,12 @@
+import 'reflect-metdata'
+import { DataSource } from 'typeorm'
+
+
+
+// export const AppDataSource =  new DataSource({
+//      type:'' ,
+//      host :'' ,
+//      username:''
+//      password:'',
+
+// })
